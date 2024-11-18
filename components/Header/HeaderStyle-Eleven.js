1037cb484821d3cef8de3b41de2b33a2f1e3d7a5
Alaguraj1 <alaguraj259@gmail.com>
2024-11-18 10:28:32 +0530

    changes

--- components/Header/HeaderStyle-Eleven.js
+++ components/Header/HeaderStyle-Eleven.js
@@ -6,7 +6,7 @@ const HeaderStyleEleven = () => {
   const { isLightTheme, toggleTheme } = useAppContext();
   return (
     <>
-      <DarkSwitch />
+      {/* <DarkSwitch /> */}
       <header className="rbt-header rbt-header-10 rbt-transparent-header">
         <div className="rbt-sticky-placeholder"></div>
         <HeaderNine
